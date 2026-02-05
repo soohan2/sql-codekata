ae82c4ff5b0ed46c9433a83f6bbe19001cbb2cb0
soohan2 <105334509+soohan2@users.noreply.github.com>
2026-02-05 21:20:01 +0900

    [프로그래머스] other - 피자 나눠 먹기 (1)

---
+++ programmers/other/120814.sql
@@ -0,0 +1,15 @@
+-- 피자 나눠 먹기 (1)
+-- 프로그래머스 (미등록 문제)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120814
+-- 작성자: 우수한
+-- 작성일: 2026. 02. 05. 21:20:00
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = (n + 6) / 7;
+    return answer;
+}
+
