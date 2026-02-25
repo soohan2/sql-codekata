e26cff3fdb94f903a8f87bb50045a5bbbde32f78
soohan2 <105334509+soohan2@users.noreply.github.com>
2026-02-26 02:25:28 +0900

    [프로그래머스] 입문 - 모든 레코드 조회하기 (#47)

---
+++ programmers/1_입문/47_모든_레코드_조회하기.sql
@@ -0,0 +1,9 @@
+-- 모든 레코드 조회하기
+-- 프로그래머스 입문 (⭐)
+-- 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/59034
+-- 작성자: 우수한
+-- 작성일: 2026. 02. 26. 02:25:27
+
+-- 코드를 입력하세요
+SELECT *
+from animal_ins
